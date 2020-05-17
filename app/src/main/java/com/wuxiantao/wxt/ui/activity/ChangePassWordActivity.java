6adf020d7b3b83317aca80e664c2e00cc4f1c42c
1820020265 <jason1985>
2020-05-17 14:38:56 +0800

    我的背包接口调试

--- app/src/main/java/com/wuxiantao/wxt/ui/activity/ChangePassWordActivity.java
+++ app/src/main/java/com/wuxiantao/wxt/ui/activity/ChangePassWordActivity.java
@@ -1,31 +1,24 @@
 package com.wuxiantao.wxt.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.kf5.sdk.system.utils.ToastUtil;
 import com.rengwuxian.materialedittext.MaterialEditText;
-import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 import com.wuxiantao.wxt.R;
 import com.wuxiantao.wxt.mvp.contract.SettingPassWordContract;
-import com.wuxiantao.wxt.mvp.presenter.MvpPresenter;
 import com.wuxiantao.wxt.mvp.presenter.SettingPassWordPresenter;
 import com.wuxiantao.wxt.mvp.view.activity.MvpActivity;
-import com.wuxiantao.wxt.ui.custom.button.StateButton;
-import com.wuxiantao.wxt.ui.title.TitleBuilder;
 import com.wuxiantao.wxt.utils.ToastUtils;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 
 import java.util.HashMap;
+
+import static com.wuxiantao.wxt.config.Constant.IS_SETPAY_PASS;
 
 /**
  * Copyright (C), 成都都爱玩科技有限公司
@@ -34,7 +27,7 @@ import java.util.HashMap;
  * Author: lht
  */
 @ContentView(R.layout.activity_change_pass_word)
-public class ChangePassWordActivity extends MvpActivity<SettingPassWordPresenter, SettingPassWordContract.ISettingView> implements TextWatcher,SettingPassWordContract.ISettingView  {
+public class ChangePassWordActivity extends MvpActivity<SettingPassWordPresenter, SettingPassWordContract.ISettingView> implements TextWatcher, SettingPassWordContract.ISettingView {
     @ViewInject(R.id.bimg_change_pwd_back)
     ImageView bimg_change_pwd_back;
     @ViewInject(R.id.change_password_old_input)
@@ -48,6 +41,7 @@ public class ChangePassWordActivity extends MvpActivity<SettingPassWordPresenter
 
     private int status = 0;
     private int type = 1;
+
     @Override
     protected SettingPassWordPresenter CreatePresenter() {
         return new SettingPassWordPresenter();
@@ -62,18 +56,18 @@ public class ChangePassWordActivity extends MvpActivity<SettingPassWordPresenter
     private void initListener() {
         bimg_change_pwd_back.setOnClickListener(v -> finish());
         change_password_submit.setOnClickListener(v -> {
-            if (status==1&&TextUtils.isEmpty(change_password_old_input.getText())){
+            if (status == 1 && TextUtils.isEmpty(change_password_old_input.getText())) {
                 ToastUtils.showToast("修改密码必须填写原密码");
-            }else if (TextUtils.isEmpty(change_password_new_input.getText())){
+            } else if (TextUtils.isEmpty(change_password_new_input.getText())) {
                 ToastUtils.showToast("请输入新密码");
-            }else {
+            } else {
                 HashMap<String, Object> map = new HashMap<>();
 //                map.put("token","o1voQ1XGQBGDT1F6UjC4xnLbFavc");
-                map.put("token",getAppToken());
-                map.put("type",type);
-                map.put("old_password",change_password_old_input.getText().toString());
-                map.put("pay_password",change_password_new_input.getText().toString());
-                map.put("pay_password_again",change_password_new_input.getText().toString());
+                map.put("token", getAppToken());
+                map.put("type", type);
+                map.put("old_password", change_password_old_input.getText().toString());
+                map.put("pay_password", change_password_new_input.getText().toString());
+                map.put("pay_password_again", change_password_new_input.getText().toString());
                 mPresenter.setUserPayPassword(map);
             }
         });
@@ -114,12 +108,16 @@ public class ChangePassWordActivity extends MvpActivity<SettingPassWordPresenter
     public void setPassWordsFailure(String failure) {
 
     }
+
     //设置交易密码成功
     @Override
     public void setPayPasswordSuccess(String msg) {
+        //保存设置密码状态
+        saveUserInfo(IS_SETPAY_PASS, "1");
         ToastUtils.showToast(msg);
         finish();
     }
+
     //设置交易密码失败
     @Override
     public void setPayPasswordFailure(String failure) {
@@ -129,15 +127,16 @@ public class ChangePassWordActivity extends MvpActivity<SettingPassWordPresenter
     //获取是否设置交易密码成功
     @Override
     public void isPayPasswordSuccess(int msg) {
-        status=msg;
-        if (msg==1){//已设置   修改
-            type=2;
+        status = msg;
+        if (msg == 1) {//已设置   修改
+            type = 2;
             change_password_title.setText("修改密码");
-        }else { //未设置  设置
-            type=1;
+        } else { //未设置  设置
+            type = 1;
             change_password_title.setText("设置密码");
         }
     }
+
     //获取是否设置交易密码失败
     @Override
     public void isPayPasswordFailure(String failure) {
